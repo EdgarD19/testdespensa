31228344e7ff66c352cb63ee63d9f82ad927a75b
EdgarD19 <edgardiosnel71@gmail.com>
2026-05-28 11:49:17 -0300

    gestion clientes

--- ejecucion.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set SPRING_DATASOURCE_URL=jdbc:postgresql://ep-ancient-silence-ac8apy42-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require
-set SPRING_DATASOURCE_USERNAME=neondb_owner
-set SPRING_DATASOURCE_PASSWORD=npg_5evWKsotDk1b
-
-.\mvnw.cmd spring-boot:run
-
-pause
